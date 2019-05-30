7975704d6eb88660204ef6c8147d4c6babc6da7c
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-30 15:08:27 -0700

    added character details to the combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import characters from './characterReducer';
+import characterDetails from './characterDetailReducer';
 
 export default combineReducers({
-  characters
+  characters,
+  characterDetails
 });
